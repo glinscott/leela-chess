e3355041553ee5d62b104ba8120c190b81864bcf
Gary Linscott <gary@zoox.com>
2018-01-09 21:22:37 -0800

    Turn off debug by default

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,7 +6,7 @@
 
 # Add -debug to see engine output
 # Missing -repeat and -openings (-t2 gives some randomness...)
-./cutechess-cli -debug -rounds 100 -tournament gauntlet -concurrency 1 \
+./cutechess-cli -rounds 100 -tournament gauntlet -concurrency 1 \
  -pgnout results.pgn \
  -engine name=lc_new cmd=/home/gary/tmp/leela-chess/src/lczero arg="-t2" arg="--weights=/home/gary/tmp/leela-chess/src/newweights.txt" \
  -engine name=lc_base cmd=/home/gary/tmp/leela-chess/src/lczero arg="-t2" arg="--weights=/home/gary/tmp/leela-chess/src/weights.txt" \
