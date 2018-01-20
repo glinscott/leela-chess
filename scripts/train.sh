d94021688290abf069144fac265878556e63fbf1
Gary Linscott <gary@zoox.com>
2018-01-19 18:31:31 -0800

    Remove unneeded step

--- scripts/train.sh
+++ scripts/train.sh
@@ -7,7 +7,6 @@ intexit() {
 
 trap intexit INT
 
-mkdir data
 ./lczero --weights=weights.txt --randomize -n -t1 --start="train 1" >training.out &
 ./lczero --weights=weights.txt --randomize -n -t1 --start="train 2" >training2.out &
 
