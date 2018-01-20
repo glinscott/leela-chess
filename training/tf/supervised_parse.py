49a855bb19c22660cd2443936459349397bf8a68
Error323 <folkerthuizinga@gmail.com>
2018-01-20 09:56:48 +0100

    Unified configfile for tensorflow training closes #41
    * Decaying lr
    * Read config from yaml
    * Multi gpu

--- training/tf/supervised_parse.py
+++ training/tf/supervised_parse.py
@@ -16,6 +16,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with Leela Zero.  If not, see <http://www.gnu.org/licenses/>.
 
+import yaml
 import sys
 import os
 import numpy as np
@@ -24,7 +25,7 @@ import tensorflow as tf
 import parse
 from tfprocess import TFProcess
 
-class Parser:
+class BinaryParser:
     def __init__(self, filename, batchsize):
         self.batch_size = batchsize
         self.example_len = 15438 # size of a tf.train.Example in binary
@@ -49,8 +50,9 @@ class Parser:
             for i in range(self.batch_size):
                 yield data[i*self.example_len:i*self.example_len+self.example_len]
 
+
 def dataset_iterator(filename, batch_size):
-    parser = Parser(filename, batch_size)
+    parser = BinaryParser(filename, batch_size)
     ds = tf.data.Dataset.from_generator(
         parser.parse_chunk, output_types=(tf.string))
     ds = ds.shuffle(65536)
@@ -60,23 +62,43 @@ def dataset_iterator(filename, batch_size):
     iterator = ds.make_one_shot_iterator()
     return iterator.get_next(), parser
 
-def main(args):
-    train_next_batch, parser = dataset_iterator(sys.argv[1], parse.BATCH_SIZE)
-    print("Creating trainingset from {}".format(sys.argv[1]))
-    num_eval = parser.num_samples() // parse.BATCH_SIZE
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: {} config.yaml".format(sys.argv[0]))
+        return 1
+
+    cfg = yaml.safe_load(open(sys.argv[1], 'r').read())
+    print(yaml.dump(cfg, default_flow_style=False))
+
+    batch_size = cfg['training']['batch_size']
+
+    filename = os.path.join(cfg['dataset']['path'], 'train.bin')
+    train_next_batch, parser = dataset_iterator(filename, batch_size)
+    print("Creating trainingset from {}".format(filename))
+    num_eval = parser.num_samples() // batch_size
     print("Train epoch in {} steps".format(num_eval))
 
-    test_next_batch, parser = dataset_iterator(sys.argv[2], parse.BATCH_SIZE)
-    print("Creating testset from {}".format(sys.argv[2]))
-    num_eval = parser.num_samples() // parse.BATCH_SIZE
+    filename = os.path.join(cfg['dataset']['path'], 'test.bin')
+    test_next_batch, parser = dataset_iterator(filename, batch_size)
+    print("Creating testset from {}".format(filename))
+    num_eval = parser.num_samples() // batch_size
     print("Test epoch in {} steps".format(num_eval))
     
-    tfprocess = TFProcess(train_next_batch, test_next_batch, num_eval)
-    if args and len(sys.argv) == 4:
-        print("Restoring neural net from {}".format(sys.argv[3]))
-        tfprocess.restore(sys.argv[3])
+    tfprocess = TFProcess(cfg, train_next_batch, test_next_batch, num_eval)
+
+    root_dir = os.path.join(cfg['training']['path'], cfg['name'])
+    if os.path.exists(os.path.join(root_dir, 'checkpoint')):
+        checkpoint = parse.get_checkpoint(root_dir)
+        tfprocess.restore(checkpoint)
+
+    if not os.path.exists(root_dir):
+        os.makedirs(root_dir)
+        print("Created output directory: {}".format(root_dir))
+
     while True:
-        tfprocess.process(parse.BATCH_SIZE)
+        tfprocess.process(batch_size)
+
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    sys.exit(main())
