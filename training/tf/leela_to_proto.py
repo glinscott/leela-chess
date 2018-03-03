af8a46e00192211f469ddd480ed3d14adcd833f3
Error323 <folkerthuizinga@gmail.com>
2018-03-03 14:12:12 +0100

    fix header

--- training/tf/leela_to_proto.py
+++ training/tf/leela_to_proto.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 #
-#    This file is part of Leela Zero.
-#    Copyright (C) 2017 Gian-Carlo Pascutto
+#    This file is part of Leela Chess.
+#    Copyright (C) 2018 Folkert Huizinga
 #
-#    Leela Zero is free software: you can redistribute it and/or modify
+#    Leela Chess is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    Leela Zero is distributed in the hope that it will be useful,
+#    Leela Chess is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with Leela Zero.  If not, see <http://www.gnu.org/licenses/>.
+#    along with Leela Chess.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 import yaml
@@ -33,7 +33,7 @@ def generate_dataset(chunks, num_samples, filename, skip, start):
     with open(filename, 'ba') as f:
         for _ in range(num_samples):
             f.write(next(gen))
-        print("Written dataset to {} pass {}/{}".format(filename, start//2, skip//2))
+        print("Written dataset to {} pass {}/{}".format(filename, start//2 + 1, skip//2))
 
 
 def main(args):
