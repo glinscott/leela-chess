7de97d22a5d705a481e83e646f3685cc1dbc41a7
jjoshua2 <jjoshua2@gmail.com>
2018-03-27 20:00:56 -0400

    Update Parameters.cpp
    I tested 2,3 and 4, and 3 was fastest for me on 1080 ti and depths 1-26 from start position.

--- src/Parameters.cpp
+++ src/Parameters.cpp
@@ -66,7 +66,7 @@ void Parameters::setup_default_parameters() {
     cfg_allow_pondering = true;
     int num_cpus = std::thread::hardware_concurrency();
     //cfg_num_threads = std::max(1, std::min(num_cpus, MAX_CPUS));
-    cfg_num_threads = 2;
+    cfg_num_threads = 3;
 
     cfg_max_playouts = 800;
     cfg_lagbuffer_cs = 100;
