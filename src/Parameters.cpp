30b2612019870585ade61e6ce3c0bd4b392a847f
jkiliani <33714573+jkiliani@users.noreply.github.com>
2018-04-06 15:11:27 +0200

    Fix cfg_puct reset

--- src/Parameters.cpp
+++ src/Parameters.cpp
@@ -80,7 +80,7 @@ void Parameters::setup_default_parameters() {
     cfg_sgemm_exhaustive = false;
     cfg_tune_only = false;
 #endif
-    cfg_puct = 0.9f;
+    cfg_puct = 0.85f;
     cfg_softmax_temp = 1.0f;
     cfg_fpu_reduction = 0.0f;
     cfg_min_resign_moves = 20;
