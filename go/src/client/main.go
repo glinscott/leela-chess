2d828f4352bbcaebd08c297d61b53b0f9cb7b97e
mike <hurnhu5622@gmail.com>
2018-04-06 17:49:12 +0200

    added ability to defind num of threads

--- go/src/client/main.go
+++ go/src/client/main.go
@@ -29,6 +29,7 @@ var USER = flag.String("user", "", "Username")
 var PASSWORD = flag.String("password", "", "Password")
 var GPU = flag.Int("gpu", -1, "ID of the OpenCL device to use (-1 for default, or no GPU)")
 var DEBUG = flag.Bool("debug", false, "Enable debug mode to see verbose output and save logs")
+var THREADS = flag.Int("threads", 2, "Define # of threads to use (2 is default)")
 
 type Settings struct {
 	User string
@@ -145,7 +146,7 @@ func (c *CmdWrapper) launch(networkPath string, args []string, input bool) {
 	c.BestMove = make(chan string)
 	weights := fmt.Sprintf("--weights=%s", networkPath)
 	dir, _ := os.Getwd()
-	c.Cmd = exec.Command(path.Join(dir, "lczero"), weights, "-t1")
+	c.Cmd = exec.Command(path.Join(dir, "lczero"), weights, fmt.Sprintf("-t%v", *THREADS))
 	c.Cmd.Args = append(c.Cmd.Args, args...)
 	if *GPU != -1 {
 		c.Cmd.Args = append(c.Cmd.Args, fmt.Sprintf("--gpu=%v", *GPU))
