e3c7fb1113c61e91cff0d48b0545e9d4600a214a
Error323 <folkerthuizinga@gmail.com>
2018-04-09 17:07:01 +0200

    add c++ conversion WIP

---
+++ training/tf/convert_v2_v3.cpp
@@ -0,0 +1,102 @@
+// g++ convert_v2_v3.cpp -std=c++14 -O3 -o convert -lz
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include <zlib.h>
+
+
+static constexpr int NUM_HIST = 8;
+static constexpr int NUM_PIECE_TYPES = 6;
+
+struct v2_t {
+  int32_t version;
+  float probs[1924];
+  uint64_t planes[112];
+  uint8_t us_ooo;
+  uint8_t us_oo;
+  uint8_t them_ooo;
+  uint8_t them_oo;
+  uint8_t stm;
+  uint8_t cnt;
+  int8_t result;
+} __attribute__((packed, aligned(4)));
+
+struct v3_t {
+  int32_t version;
+  float probs[1858];
+  uint64_t planes[104];
+  uint8_t us_ooo;
+  uint8_t us_oo;
+  uint8_t them_ooo;
+  uint8_t them_oo;
+  uint8_t stm;
+  uint8_t cnt;
+  int8_t result;
+} __attribute__((packed, aligned(4)));
+
+
+uint64_t reverse(uint64_t x) {
+  auto a = reinterpret_cast<char*>(&x);
+  uint64_t r;
+  auto b = reinterpret_cast<char*>(&r);
+  for (int i = 0; i < 4; i++) {
+    char tmp = a[i];
+    a[i] = b[7-i];
+    b[7-i] = tmp;
+  }
+
+  return r;
+}
+
+
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    std::cout << "usage: " << argv[0] << " in.gz out.gz" << std::endl;
+    return 1;
+  }
+
+  gzFile infile = gzopen(argv[1], "rb");
+  gzFile outfile = gzopen(argv[2], "wb");
+
+  v2_t v2;
+  v3_t v3;
+  v3.version = 3;
+  int count = 0;
+
+  while (gzread(infile, reinterpret_cast<char*>(&v2), sizeof(v2)) > 0) {
+    if (v2.version != 2) {
+      throw std::runtime_error("Invalid version");
+    }
+
+    // probabilities
+
+    // planes
+    for (int i = 0; i < NUM_HIST; i++) {
+      for (int j = 0; j < NUM_PIECE_TYPES; j++) {
+        if (i&1) {
+          v3.planes[i*13+j] = reverse(v2.planes[i*14+j]);
+        }
+        else {
+          v3.planes[i*13+j] = v2.planes[i*14+j];
+        }
+      }
+    }
+
+    v3.us_ooo = v2.us_ooo;
+    v3.us_oo = v2.us_oo;
+    v3.them_ooo = v2.them_ooo;
+    v3.them_oo = v2.them_oo;
+    v3.stm = v2.stm;
+    v3.cnt = v2.cnt;
+    v3.result = v2.result;
+    gzwrite(outfile, reinterpret_cast<char*>(&v3), sizeof(v3));
+    count++;
+  };
+
+  gzclose(infile);
+  gzclose(outfile);
+
+  return 0;
+}
