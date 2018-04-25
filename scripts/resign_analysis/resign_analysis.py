9d365ba2b3bf024ad1bc642665529c6020b9dc9e
Andy Olsen <andy.olsen.tx@gmail.com>
2018-04-25 18:29:37 -0500

    Fix bug in correct_resigns.
    Also output more stats.

--- scripts/resign_analysis/resign_analysis.py
+++ scripts/resign_analysis/resign_analysis.py
@@ -28,6 +28,7 @@ def parseGames(filenames, resignrate):
     very_incorrect_resigns = 0
     total_plys = 0
     resign_plys = 0
+    draws = 0
     for filename in filenames:
         #print("debug filename: {}".format(filename))
         plynum = 0
@@ -60,20 +61,33 @@ def parseGames(filenames, resignrate):
             incorrect_resigns += 1
             #print("debug incorrect resign filename {} who_resigned {} resign_playnum {} total_plynum {}".format(
                 #filename, who_resigned, resign_plynum, plynum))
+        elif who_resigned != None:
+            #print("debug correct resign")
+            correct_resigns += 1
         if ((score == -1 and who_resigned == "Black") or
             (score ==  1 and who_resigned == "White")):
             very_incorrect_resigns += 1
             #print("debug very incorrect resign filename {} who_resigned {} resign_playnum {} total_plynum {}".format(
                 #filename, who_resigned, resign_plynum, plynum))
-        elif who_resigned != None:
-            #print("debug correct resign")
-            correct_resigns += 1
-    print("Resign rate {:2.0f}% Incorrect {:4.1f}% Very Incorrect {:4.1f}% Correct {:4.1f}% Plies saved {:4.1f}%".format(
-        resignrate,
-        incorrect_resigns/games*100,
-        very_incorrect_resigns/games*100,
-        correct_resigns/games*100,
-        (total_plys-resign_plys)/total_plys*100))
+        if score == 0:
+            draws += 1
+    assert incorrect_resigns >= very_incorrect_resigns
+    assert incorrect_resigns + correct_resigns <= games
+    resigned_games = incorrect_resigns + correct_resigns
+    if resigned_games == 0:
+        print("RR {:2.0f}% NR 100%".format(resignrate))
+    else:
+        print("RR {:2.0f}% Draw {:4.1f}% NR {:4.1f}% I {:4.1f}%/{:4.1f}% VI {:4.1f}%/{:4.1f}% C {:4.1f}%/{:4.1f}% PS {:4.1f}%".format(
+            resignrate,
+            draws/games*100,
+            (games - resigned_games)/games*100,
+            incorrect_resigns/games*100,
+            incorrect_resigns/resigned_games*100,
+            very_incorrect_resigns/games*100,
+            very_incorrect_resigns/resigned_games*100,
+            correct_resigns/games*100,
+            correct_resigns/resigned_games*100,
+            (total_plys-resign_plys)/total_plys*100))
 
 if __name__ == "__main__":
     usage_str = """
@@ -90,7 +104,13 @@ Process flow:
     parser.add_argument("data", metavar="files", type=str, nargs="+", help="client debug log files (logs-###/*.log)")
     args = parser.parse_args()
     print("Analyzing {} games".format(len(args.data)))
-    print("Incorrect resign = would have resigned, but drew or won.")
-    print("Very Incorrect resign = would have resigned, won.")
+    print("RR   - Resign Rate")
+    print("Draw - number of drawn games")
+    print("NR   - No Resign")
+    print("I    - Incorect Resign (would have resigned, but drew or won)")
+    print("VI   - Very Incorect Resign (would have resigned, but won)")
+    print("C    - Correct Resign (would have resigned, and actually lost")
+    print("PS   - Plies Saved")
+    print("I, VI, C display two values. Including / excluding the NR games")
     for rr in (0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 10.0, 20.0, 30.0, 40.0, 50.0):
         parseGames(args.data, rr)
