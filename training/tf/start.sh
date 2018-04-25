a7a2fbfa332d442c5077695491ae0d50b51aafbd
Error323 <folkerthuizinga@gmail.com>
2018-04-25 22:11:15 +0300

    live training parameters

--- training/tf/start.sh
+++ training/tf/start.sh
@@ -5,18 +5,19 @@ set -e
 CONFIG=$1
 ADDRESS=$2
 NET="/tmp/weights.txt"
+NETDIR="/home2/networks/upload"
 
 while true
 do
-  ./train.py --cfg=$CONFIG --output=$NET | tee $HOME/logs/$(date +%Y%m%d-%H%M%S).log
+  unbuffer ./train.py --cfg=$CONFIG --output=$NET 2>&1 | tee /home2/logs/$(date +%Y%m%d-%H%M%S).log
 
   # prepare network for uploading
   CHECKSUM=$(sha256sum $NET | awk '{print $1}')
-  mv $NET $CHECKSUM
-  gzip -9 $CHECKSUM
-  FILE="$CHECKSUM.gz"
+  mv -v $NET $NETDIR/$CHECKSUM
+  gzip -9 $NETDIR/$CHECKSUM
+  FILE="$NETDIR/$CHECKSUM.gz"
 
   # upload in the background and continue next training session
   echo "Uploading '$FILE' to $ADDRESS"
-  curl -s -F "file=@${FILE}" -F "training_id=1" -F "layers=6" -F "filters=64" $ADDRESS &
+  curl -s -F "file=@${FILE}" -F "training_id=1" -F "layers=10" -F "filters=128" $ADDRESS &
 done
