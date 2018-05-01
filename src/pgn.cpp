bd997e3a27637a3a79934cd0c5d7b2c11771c0fa
samu <samuel.gravan@starshotsoftware.com>
2018-05-01 09:13:52 +0200

    Adding drop annotations & removing some tabs

--- src/pgn.cpp
+++ src/pgn.cpp
@@ -63,21 +63,40 @@ std::unique_ptr<PGNGame> PGNParser::parse() {
       continue;
     }
 
+    if (s[0] == '[') {
+      do {
+        is_ >> s;
+      } while (s.back() != ']');
+      continue;
+    }
+
     // Skip the move numbers
     if ((i % 3) == 0) {
-      // Some PGN files have this movement format: 1.d4 (no space after the dot) instead of 1. d4 
+      // Some PGN files have this movement format: 1.d4 (no space after the dot) instead of 1. d4
       if (s.back() != '.')
-      {		
+      {
         size_t pos = 0;
         if ((pos = s.find(delimiter)) != std::string::npos) {
           s = s.substr(pos + 1, std::string::npos);
           ++i;
-        }		
-	  }	  
+        }
+	  }
       else {
         ++i;
         continue;
-      }            
+      }
+    }
+    
+	// Drop annotations
+    if (s.back() == '!' || s.back() == '?') {
+      size_t len = s.length();
+      auto backIt = s.end();
+      backIt--;
+      while (*backIt == '!' || *backIt == '?') {
+        backIt--;
+        len--;
+      }
+      s = s.substr(0, len);
     }
 
     ++i;
