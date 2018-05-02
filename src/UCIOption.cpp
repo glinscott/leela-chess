e6c4e8eb06eac8ab081e3d103bf1046b7cc7f9ae
jjoshua2 <jjoshua2@gmail.com>
2018-05-02 15:44:02 -0400

    set cfg_syzygydraw with uci
    use cfg_syzygydraw

--- src/UCIOption.cpp
+++ src/UCIOption.cpp
@@ -59,7 +59,19 @@ namespace UCI {
 
         cfg_quiet = value;
     }
+  
+    void on_syzygydraw(const Option& o) {
+        bool value = o;
 
+        if (value) {
+            myprintf("Enabled using syzygy tb draws\n", value);
+        } else {
+            myprintf("Disabled using syzygy tb draws\n", value);
+        }
+
+        cfg_syzygydraw = value;
+    }
+  
     bool set_float_cfg(float& cfg_param, const std::string& value) {
         try {
             cfg_param = std::strtof(value.c_str(), nullptr);
@@ -138,6 +150,7 @@ namespace UCI {
     void init(OptionsMap& o) {
         o["Threads"]                << Option(cfg_num_threads, 1, cfg_max_threads, on_threads);
         o["Quiet"]                  << Option(cfg_quiet, on_quiet);
+        o["Quiet"]                  << Option(cfg_syzygydraw, on_syzygydraw);
         o["Softmax Temp"]           << SilentOption(std::to_string(cfg_softmax_temp).c_str(), on_softmaxtemp);
         o["FPU Reduction"]          << Option(std::to_string(cfg_fpu_reduction).c_str(), on_fpureduction);
         o["FPU Dynamic Eval"]       << SilentOption(cfg_fpu_dynamic_eval, on_fpudynamiceval);
