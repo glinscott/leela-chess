5366cbc6127d5b348596738b29dacab21448da1e
Gian-Carlo Pascutto <gcp@sjeng.org>
2018-05-05 00:20:24 +0200

    Use boost::spirit for weights parsing. (#516)
    Using boost::Spirit::X3 for weights parsing is significantly faster in
    startup. Current startup time goes from 3.5s to 2.8s.
    
    See also:
    https://github.com/gcp/leela-zero/pull/805

--- src/Network.cpp
+++ src/Network.cpp
@@ -34,6 +34,7 @@
 #include <thread>
 #include <boost/utility.hpp>
 #include <boost/format.hpp>
+#include <boost/spirit/home/x3.hpp>
 #include "zlib.h"
 
 #ifdef __APPLE__
@@ -60,6 +61,7 @@
 #include "ThreadPool.h"
 #include "Im2Col.h"
 
+namespace x3 = boost::spirit::x3;
 using namespace Utils;
 
 // Seems this is required for the Darwin compiler.
@@ -262,18 +264,10 @@ std::pair<int, int> Network::load_network(std::istream& wtfile) {
     linecount = 0;
     while (std::getline(wtfile, line)) {
         std::vector<float> weights;
-        float weight;
-        char * fz = &line[0];
-        bool ok = true;
-        for (; *fz != '\0';) {
-            char * tmp = fz;
-            weight = strtof(fz, &fz); // if the read fails, fz is unchanged and weight is 0.0F.
-            if (weight == 0.0F && tmp == fz) {
-                ok = false; break;
-            }
-            weights.emplace_back(weight);
-        }
-        if (!ok) {
+        auto it_line = cbegin(line);
+        const auto ok = phrase_parse(it_line, cend(line),
+                                     *x3::float_, x3::space, weights);
+        if (!ok || it_line != cend(line)) {
             myprintf("\nFailed to parse weight file. Error on line %d.\n",
                     linecount + 2); //+1 from version line, +1 from 0-indexing
             return {0, 0};
