172f925cbf20797b01f6af2cb9e69d5598dfd4c9
Error323 <folkerthuizinga@gmail.com>
2018-05-17 09:09:46 +0200

    improve build.sh

--- lc0/build.sh
+++ lc0/build.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -e
+
+BUILDTYPE=$1
+
+if [ -z ${BUILDTYPE} ]
+then
+  BUILDTYPE=release
+fi
 
 rm -fr build
-meson build --buildtype release # -Db_ndebug=true
-# CC=clang CXX=clang++ meson build --buildtype debugoptimized -Db_asneeded=false
-# CC=clang CXX=clang++ meson build --buildtype debug
-cd build
+meson build --buildtype ${BUILDTYPE}
+
+pushd build
 ninja
+popd
