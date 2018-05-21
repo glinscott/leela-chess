ce48dcfec109ae21979dc0ad42c5b96e61d1013e
Dubslow <bunslow@gmail.com>
2018-05-21 03:28:57 -0400

    More work. Top level OpenCLNetwork stuff is ~complete

--- lc0/src/neural/network_blas_cl_common.cc
+++ lc0/src/neural/network_blas_cl_common.cc
@@ -17,6 +17,23 @@
     along with Leela Zero.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/*
+get_input_channels() -> kInputPlanes, Network::initialize::channels -> ConvBlock.biases.size()
+conv_weights -> ConvBlock.weights, conv_biases -> ConvBlock.biases
+batchnorm_means -> ConvBlock.bn_means, batchnorm_stddivs -> ConvBlock.bn_stddivs
+conv_pol_w -> Weights.policy.weights, conv_pol_b -> Weights.policy.biases
+bn_pol_w1 -> Weights.policy.bn_means, bn_pol_w2 -> Weights.policy.bn_stddivs // ??? not very useful oldnames
+NUM_POLICY_INPUT_PLANES -> Weights.policy.bn_means.size()
+ip_pol_w -> Weights.ip_pol_w, ip_pol_b -> Weights.ip_pol_b
+get_num_output_policy() -> Weights.ip_pol_b.size()
+conv_val_w -> Weights.value.weights, conv_val_b -> Weights.value.biases
+bn_val_w1 -> Weights.value.bn_means, bn_val_w2 -> Weights.value.bn_stddivs
+NUM_VALUE_INPUT_PLANES -> Weights.value.bn_means.size()
+{ip1_val_w,ip1_val_b,ip2_val_w,ip2_val_b} -> Weights.{}
+NUM_VALUE_CHANNELS -> Weights.ip1_val_b.size()
+
+*/
+
 #pragma once
 
 #include "factory.h" // network.h, optionsdict.h
@@ -39,7 +56,7 @@ class BlasCLNetworkComputation : public NetworkComputation {
     output_values = std::make_unique<float[]>(inputs.size());
     output_policies = std::make_unique<float[][]>(inputs.size());
     for (size_t i = 0; i < inputs.size(); i++)
-      output_values[i], output_policies[i] = network->forward(inputs[i]);
+      output_values[i], output_policies[i] = network->evaluate(inputs[i]);
   }
   
   int GetQVal(int sample) override {
@@ -54,34 +71,44 @@ class BlasCLNetworkComputation : public NetworkComputation {
   std::vector<InputPlanes> inputs;
   float[]   output_values;
   float[][] output_policies;
-  BlasNetwork* network;
+  BlasCLNetwork* network;
 }
 
 class BlasCLNetwork : public Network {
  public:
   BlasCLNetwork(const Weights& weights, const OptionsDict& options)
     : weights(weights), options(options) {
-    // this matches old Network::initialize, in Network.cpp:375-416   
-    initOneBlock(weights.input, true);
-    for (auto& resblock : weights.residual)
-      initOneBlock(resblock);
-    initOneBlock(weights.policy);
-    initOneBlock(weights.value);
+    initialize();
   }
     
   std::unique_ptr<NetworkComputation> NewComputation() override {
     return std::make_unique<BlasCLNetworkComputation>(this);
   }
-  virtual std::pair<float value, float[] policy> forwardEval(InputPlanes&& input);
-  // output array should be std::make_unique
+  
+  virtual std::pair<float value, float[] policy> evaluate(InputPlanes&& input);
+  // output array should be std::make_unique  
   
  protected:
-  virtual void initOneBlock(Weights::ConvBlock& block, bool inputlayer=false);
-  Weights::Vec winograd_transform_f(const Weights::Vec& f, const int outputs, const int channels);
+  virtual void forward(...);
   void softmax();
-
+ 
+  void initialize(void);
+  void initOneBlock(Weights::ConvBlock& block, bool inputlayer=false);
+  Weights::Vec winograd_transform_f(const Weights::Vec& f, const int outputs, const int channels);
+  
   Weights weights; // optimal memory use? is one reference shared among multiple backends?
   OptionsDict& options;
+}
+
+void BlasCLNetwork::initialize(void) {
+  // this matches old Network::initialize, in Network.cpp:375-416   
+  initOneBlock(weights.input, true);
+  for (auto& resblock : weights.residual) {
+    initOneBlock(resblock.conv1);
+    initOneBlock(resblock.conv2);
+  }
+  initOneBlock(weights.policy);
+  initOneBlock(weights.value);
 }
 
 void BlasCLNetwork::initOneBlock(Weights::ConvBlock& block, bool inputlayer=false) {
@@ -104,6 +131,26 @@ void BlasCLNetwork::initOneBlock(Weights::ConvBlock& block, bool inputlayer=fals
 
 Weights::Vec BlasCLNetwork::winograd_transform_f(const Weights::Vec& f, const int outputs, const int channels) {
 
+}
+
+std::pair<float value, float[] policy> evaluate(InputPlanes&& input) {
+  // something something convert input data
+  forward(input_data, policy_data, value_data); // virtual
+  
+  // Get the moves
+  softmax(policy_data, softmax_data, cfg_softmax_temp);
+  std::vector<float>& outputs = softmax_data;
+
+  // Now get the score
+  innerproduct<NUM_VALUE_CHANNELS, 1>(value_data, ip2_val_w, ip2_val_b, winrate_out);
+  
+  // blah blah further processing
+  
+  float[] policy = std::make_unique<float[]>();
+  
+  // blah blah
+  
+  return {value, policy};
 }
 
 void BlasCLNetwork::softmax();
--- lc0/src/neural/network_opencl.cc
+++ lc0/src/neural/network_opencl.cc
@@ -23,20 +23,91 @@
 
 namespace lczero {
 
-class OpenCLNetwork : public BlasNetwork {
+class OpenCLNetwork : public BlasCLNetwork {
  public:
   OpenCLNetwork(const Weights& weights, const OptionsDict& options)
-    : BlasNetwork(weights, options) {
-    //myprintf("Initializing OpenCL.\n");
-    opencl.initialize(weights.input.biases.size());
-    // etc, tbd (refactoring necessary?)
+    : BlasCLNetwork(weights, options) {
+    initialize();
   }
-  std::pair<float value, float[] policy> forward(InputPlanes&& input) override;
+  std::pair<float value, float[] policy> forwardEval(InputPlanes&& input) override;
 
  protected:
-  void initOneBlock(Weights::ConvBlock& block, bool inputlayer=false) override;
+  void initialize(void);
+  std::vector<float> zeropad_U(const std::vector<float>& U, const int outputs, const int channels, const int outputs_pad, const int channels_pad);
+  OpenCLSchedular opencl;
 }
 
+void initialize(void) {
+  // this function corresponds to Network.cpp:418-496
+  //myprintf("Initializing OpenCL.\n");
+  channels = weights.input.biases.size();
+  opencl.initialize(channels);
+  
+  for (auto& opencl_net : opencl.get_networks()) {
+    auto tuners = opencl_net->getOpenCL().get_sgemm_tuners();
+    auto mwg = tuners[0];
+    auto kwg = tuners[2];
+    auto vwm = tuners[3];
+    size_t m_ceil = ceilMultiple(ceilMultiple(channels, mwg), vwm);
+    size_t k_ceil = ceilMultiple(ceilMultiple(get_input_channels(), kwg), vwm);
+
+    auto Upad = zeropad_U(weights.input.weights, channels, kInputPlanes, m_ceil, k_ceil);
+
+    opencl_net->push_input_convolution(WINOGRAD_ALPHA, kInputChannels, channels,
+                                       Upad, weights.input.bn_means, weights.input.bn_stddivs);
+
+    for (auto& resblock : weights.residual) {
+      auto Upad1 = zeropad_U(resblock.conv1.weights, channels, channels, m_ceil, m_ceil);
+      auto Upad2 = zeropad_U(resblock.conv2.weights, channels, channels, m_ceil, m_ceil);
+      opencl_net->push_residual(WINOGRAD_ALPHA, channels, channels,
+                                Upad1, resblock.conv1.bn_means, resblock.conv1.bn_stddivs,
+                                Upad2, resblock.conv2.bn_means, resblock.conv2.bn_stddivs);
+    }
+    
+    constexpr unsigned int width = 8;
+    constexpr unsigned int height = 8;
+    const auto num_p_inputs  = weights.policy.bn_means.size(); // NUM_POLICY_INPUT_PLANES
+    const auto num_p_outputs = weights.ip_pol_b.size()         // get_num_output_policy()
+    const auto num_v_inputs  = weights.value.bn_means.size();  // NUM_VALUE_INPUT_PLANES
+    const auto num_v_outputs = weights.ip1_val_b.size();       // NUM_VALUE_CHANNELS
+    
+    opencl_net->push_policy(channels, num_p_inputs, num_p_inputs*width*height, num_p_outputs,
+                            weights.policy.weights,
+                            weights.policy.bn_means, weights.policy.bn_stddivs,
+                            weights.ip_pol_w, weights.ip_pol_b);
+
+    opencl_net->push_value (channels, num_v_inputs, num_v_inputs*width*height, num_v_outputs,
+                            weights.value.weights,
+                            weights.value.bn_means, weights.value.bn_stddivs,
+                            weights.ip1_val_w, weights.ip1_val_b);
+                            
+  }
+}
+
+std::vector<float> OpenCLNetwork::zeropad_U(const std::vector<float>& U, const int outputs, const int channels, const int outputs_pad, const int channels_pad) {
+  // Fill with zeroes
+  auto Upad = std::vector<float>(WINOGRAD_TILE * outputs_pad * channels_pad);
+
+  for(auto o = 0; o < outputs; o++) {
+      for(auto c = 0; c < channels; c++) {
+          for(auto xi = 0; xi < WINOGRAD_ALPHA; xi++){
+              for(auto nu = 0; nu < WINOGRAD_ALPHA; nu++) {
+                  Upad[xi * (WINOGRAD_ALPHA * outputs_pad * channels_pad)
+                     + nu * (outputs_pad * channels_pad)
+                     + c * outputs_pad +
+                       o]
+                  =
+                  U  [xi * (WINOGRAD_ALPHA * outputs * channels)
+                    + nu * (outputs * channels)
+                    + c * outputs
+                    + o];
+              }
+          }
+      }
+  }
+
+  return Upad;
+}
 
 REGISTER_NETWORK("opencl", OpenCLNetwork, 90);
 
