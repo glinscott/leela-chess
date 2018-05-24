ff7d19e95915fffeb1a9d3681c9831dea1c60486
Francois Pays <frpays@gmail.com>
2018-05-24 22:32:26 +0200

    Meson build for opencl

--- lc0/src/neural/opencl/OpenCL.h
+++ lc0/src/neural/opencl/OpenCL.h
@@ -24,7 +24,7 @@
 #define CL_HPP_MINIMUM_OPENCL_VERSION   110
 #define CL_HPP_TARGET_OPENCL_VERSION    120
 #define CL_HPP_ENABLE_EXCEPTIONS
-#include <CL/cl2.hpp>
+#include "CL/cl2.hpp"
 #include <cstddef>
 #include <memory>
 #include <string>
