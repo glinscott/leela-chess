7220bd35e4f726b76065f37e30fb070e93302a3f
Dubslow <bunslow@gmail.com>
2018-05-30 23:49:06 -0400

    Refocus repository to just lczero engine

--- go/src/server/prod.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# You must have run `go build main.go` prior to running this
-
-export GIN_MODE=release
-./main
--- go/src/server/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-go build main.go
-pkill -f main
-nohup ./prod.sh & >server.out
